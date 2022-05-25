7dbfaee539406e438a622717ef5c988099bbff50
Debbie Neflas <debbieneflas@gmail.com>
2022-05-25 12:25:05 -0700

    update connection config

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 //create a connection to our database
 let sequelize;
 
-if (process.env.JAWSDB.URL) {
-    sequelize = new Sequelize(process.env.JAWSDB.URL);
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else{
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: 'localhost',
