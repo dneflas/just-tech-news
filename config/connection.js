708ae16b1415c56dfac2b61f0bdfbfb3ed54ed77
Debbie Neflas <debbieneflas@gmail.com>
2022-05-25 12:18:15 -0700

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,17 @@ const Sequelize = require ('sequelize');
 require('dotenv').config();
 
 //create a connection to our database
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+let sequelize;
+
+if (process.env.JAWSDB.URL) {
+    sequelize = new Sequelize(process.env.JAWSDB.URL);
+} else{
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+};
+
 
 module.exports = sequelize;
