b9ebddc0918f9c85de9ae21c2dba1dac077e21d8
Debbie Neflas <debbieneflas@gmail.com>
2022-06-01 12:26:03 -0700

    created user login

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+
+const sequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new sequelizeStore({
+    db: sequelize,
+  }),
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,13 +26,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
 
 // turn on connection and db server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
+  app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
 });
-
-
