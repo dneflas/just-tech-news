09605ffed7467239406becec3702141336d9db45
Debbie Neflas <debbieneflas@gmail.com>
2022-06-03 11:47:04 -0700

    Dashboard view

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User, Post, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
 
 // get all Post
 router.get('/', (req, res) => {
@@ -88,11 +89,11 @@ router.get('/:id', (req, res) => {
 });
 
 // create a post
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
   Post.create({
     title: req.body.title,
     post_url: req.body.post_url,
-    user_id: req.body.user_id,
+    user_id: req.session.user_id,
   })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
@@ -106,7 +107,6 @@ router.put('/upvote', (req, res) => {
   // make sure the session exists first
   if (req.session) {
     // pass session id along with all destructured properties on req.body
-    console.log(req.session);
     Post.upvote({ ...req.body, user_id: req.session.user_id })
       .then((updatedVoteData) => res.json(updatedVoteData))
       .catch((err) => {
@@ -117,7 +117,7 @@ router.put('/upvote', (req, res) => {
 });
 
 // update a post
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
   Post.update(
     {
       title: req.body.title,
@@ -142,7 +142,7 @@ router.put('/:id', (req, res) => {
 });
 
 // delete a post
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
   Post.destroy({
     where: {
       id: req.params.id,
--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 // if a request is made to endpoint that doesn't exist, a 404 error will occur
 router.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 module.exports = router;
