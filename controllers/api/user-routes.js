dadb16a5c0ac32c3b8a464c322ae9db1e36529c5
Debbie Neflas <debbieneflas@gmail.com>
2022-06-02 17:18:17 -0700

    upvote and comment functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -98,7 +98,7 @@ router.post('/login', (req, res) => {
       return;
     }
     req.session.save(() => {
-      req.session.id = dbUserData.id;
+      req.session.user_id = dbUserData.id;
       req.session.username = dbUserData.username;
       req.session.loggedIn = true;
 
--- server.js
+++ server.js
@@ -1,32 +1,35 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const path = require('path');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const express = require('express');
 const session = require('express-session');
+const routes = require('./controllers');
+const exphbs = require('express-handlebars');
 
-const sequelizeStore = require('connect-session-sequelize')(session.Store);
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
-  store: new sequelizeStore({
+  store: new SequelizeStore({
     db: sequelize,
   }),
 };
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+app.use(session(sess));
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.use(session(sess));
 
 // turn on routes
 app.use(routes);
